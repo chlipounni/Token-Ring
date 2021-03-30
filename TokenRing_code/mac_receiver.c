1069fffb55fe076b29317236e756f0720ecb4ba7
chlipounni <50593554+chlipounni@users.noreply.github.com>
2021-03-30 14:25:15 +0200

    add newToken fonction

--- TokenRing_code/mac_receiver.c
+++ TokenRing_code/mac_receiver.c
@@ -11,13 +11,56 @@
 #include <string.h>
 #include "main.h"
 
+void sendToken (uint8_t * msg);
 
 //////////////////////////////////////////////////////////////////////////////////
 // THREAD MAC RECEIVER
 //////////////////////////////////////////////////////////////////////////////////
 void MacReceiver(void *argument)
 {
+	struct queueMsg_t queueMsg;					// queue message
+	osStatus_t retCode;									// return error code
+	
+	uint8_t * qPtr;
+	
+		//------------------------------------------------------------------------------
+	for (;;)														// loop until doomsday
+	{
+		//----------------------------------------------------------------------------
+		// QUEUE READ									
+		//----------------------------------------------------------------------------
+		retCode = osMessageQueueGet( 	
+			queue_macR_id,
+			&queueMsg,
+			NULL,
+			osWaitForever); 
+    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);
+		
+		qPtr = queueMsg.anyPtr;
+		
+		if(qPtr[0] == TOKEN_TAG){
+			// this is a token !!!!!!!!
+			sendToken(qPtr);
+		}			
+	}	
+}
 
+void sendToken (uint8_t * msg){
+	struct queueMsg_t queueMsg;
+	osStatus_t retCode;
+	
+	queueMsg.type = TOKEN;
+	queueMsg.anyPtr = msg;
+	queueMsg.addr = NULL;
+	queueMsg.sapi = NULL;
+	
+	retCode = osMessageQueuePut(
+		queue_macS_id,
+		&queueMsg,
+		osPriorityNormal,
+		osWaitForever);
+	
+	CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);		
 }
 
 
--- TokenRing_code/mac_sender.c
+++ TokenRing_code/mac_sender.c
@@ -12,13 +12,92 @@
 #include "main.h"
 #include "ext_led.h"
 
+void sendToPhy (uint8_t * msg);
+
 
 //////////////////////////////////////////////////////////////////////////////////
 // THREAD MAC RECEIVER
 //////////////////////////////////////////////////////////////////////////////////
 void MacSender(void *argument)
 {
+	uint8_t * msg;
+	struct queueMsg_t queueMsg;					// queue message
+	osStatus_t retCode;									// return error code
+	
+		//------------------------------------------------------------------------------
+	for (;;)														// loop until doomsday
+	{
+		//----------------------------------------------------------------------------
+		// QUEUE READ									
+		//----------------------------------------------------------------------------
+		retCode = osMessageQueueGet( 	
+			queue_macS_id,
+			&queueMsg,
+			NULL,
+			osWaitForever); 
+    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);
+		
+		switch (queueMsg.type){
+			
+			case DATA_IND :
 
+			break;
+			
+			case TOKEN:
+			Ext_LED_PWM(2,100);										// token is in station
+			break;
+			
+			case DATABACK:
+				
+			break;
+			
+			case NEW_TOKEN:
+
+				msg = osMemoryPoolAlloc(memPool,osWaitForever);
+				msg[0] = TOKEN_TAG;
+			
+				for(int i = 1; i<TOKENSIZE-3;i++){
+					msg[i] = 0x00;
+				}
+				
+				sendToPhy(msg);
+				
+			break;
+			
+			case START:
+				
+			break;
+			
+			case STOP:
+				
+			break;
+			
+			default:
+				
+			break;
+		
+		
+		};						
+	}	
 }
 
+
+void sendToPhy (uint8_t * msg){
+	struct queueMsg_t queueMsg;
+	osStatus_t retCode;
+	
+	queueMsg.type = TO_PHY;
+	queueMsg.anyPtr = msg;
+	queueMsg.addr = NULL;
+	queueMsg.sapi = NULL;
+	
+	retCode = osMessageQueuePut(
+		queue_phyS_id,
+		&queueMsg,
+		osPriorityNormal,
+		osWaitForever);
+	
+	CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);		
+}
+	
 
